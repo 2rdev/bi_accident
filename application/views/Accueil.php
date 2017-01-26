b861cd330043c04dfdfebf58ef80521d8408ea0a
2rdev <pro2rdev@gmail.com>
2017-01-26 15:13:22 +0100

    Commit

---
+++ application/views/Accueil.php
@@ -0,0 +1,8 @@
+ <div class="container theme-showcase" role="main">
+
+      <!-- Main jumbotron for a primary marketing message or call to action -->
+      <div class="jumbotron">
+        <h1>Projet BI</h1>
+        <p>En attente de la BD</p>
+      </div>
+</div>
